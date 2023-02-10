87ba0164f1bf7512dec69bce27165f8ebf2bbcb9
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-02-10 16:15:29 +0100

    container

--- hubble-page/src/App.js
+++ hubble-page/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import {Container} from './components/styles/Container.styled'
 
 function App() {
   return (
     <div className="App">
+    <Container>
+    <h1>Hello</h1>
+    </Container>
     </div>
   );
 }
